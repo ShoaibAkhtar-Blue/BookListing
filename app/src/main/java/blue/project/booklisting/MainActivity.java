da3810a5149517db4ab429079ccffbaedb10bac9
Shoaib Akhtar <85933802+ShoaibAkhtar-Blue@users.noreply.github.com>
2021-07-27 15:07:42 +0500

    Fix font and splash screen

--- app/src/main/java/blue/project/booklisting/MainActivity.java
+++ app/src/main/java/blue/project/booklisting/MainActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Hide status bar
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        //getWindow().setStatusBarColor(R.color.white);
 
         // Hide App/Action bar
         getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
 
         // Set delay for splash screen
         Handler handler = new Handler();
